395af1a534ade549e46e55a8b1abbd3983a01734
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-12-31 13:46:05 +0530

    MOVED HARDCODED ROUTES INTO CONSTANTS FILE.

--- app/src/main/java/com/bhardwaj/pokemon/navigation/Screen.kt
+++ app/src/main/java/com/bhardwaj/pokemon/navigation/Screen.kt
@@ -1,13 +1,20 @@
 package com.bhardwaj.pokemon.navigation
 
+import com.bhardwaj.pokemon.utils.Constants.DETAILS_ARGUMENT_KEY
+import com.bhardwaj.pokemon.utils.Constants.ROUTE_DETAILS_SCREEN
+import com.bhardwaj.pokemon.utils.Constants.ROUTE_HOME_SCREEN
+import com.bhardwaj.pokemon.utils.Constants.ROUTE_SEARCH_SCREEN
+import com.bhardwaj.pokemon.utils.Constants.ROUTE_SPLASH_SCREEN
+import com.bhardwaj.pokemon.utils.Constants.ROUTE_WELCOME_SCREEN
+
 sealed class Screen(val route: String) {
-    object Splash : Screen("splash_screen")
-    object Welcome : Screen("welcome_screen")
-    object Home : Screen("home_screen")
-    object Search : Screen("search_screen")
-    object Details : Screen("details_screen/{heroId}") {
+    object Splash : Screen(ROUTE_SPLASH_SCREEN)
+    object Welcome : Screen(ROUTE_WELCOME_SCREEN)
+    object Home : Screen(ROUTE_HOME_SCREEN)
+    object Search : Screen(ROUTE_SEARCH_SCREEN)
+    object Details : Screen("$ROUTE_DETAILS_SCREEN/{$DETAILS_ARGUMENT_KEY}") {
         fun passHeroId(heroId: Int): String {
-            return "details_screen/$heroId"
+            return "$ROUTE_DETAILS_SCREEN/$heroId"
         }
     }
 }
--- app/src/main/java/com/bhardwaj/pokemon/utils/Constants.kt
+++ app/src/main/java/com/bhardwaj/pokemon/utils/Constants.kt
@@ -1,5 +1,11 @@
 package com.bhardwaj.pokemon.utils
 
 object Constants {
+    const val ROUTE_SPLASH_SCREEN = "route_splash_screen"
+    const val ROUTE_WELCOME_SCREEN = "route_welcome_screen"
+    const val ROUTE_HOME_SCREEN = "route_home_screen"
+    const val ROUTE_SEARCH_SCREEN = "route_search_screen"
+    const val ROUTE_DETAILS_SCREEN = "details_screen"
+
     const val DETAILS_ARGUMENT_KEY = "heroId"
 }
